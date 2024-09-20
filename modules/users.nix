a168995d35b070edda9e0759359ab8f02d361d21
JacobWarners <jacob.warners@numbusnetworking.net>
2024-09-19 22:42:16 -0700

    2024-09-19 22:42:16

--- modules/users.nix
+++ modules/users.nix
@@ -7,15 +7,12 @@
     shell = pkgs.zsh;
     initialHashedPassword = "$y$j9T$MkzXr4RqnBYu92A6DSwJv1$Hxj3vUPY3vCvPyJ1Z8vfaQhSEn4ZO0vKNsJYhpnJkF.";
     extraGroups = [ "wheel" "docker" "podman" ];
-    openssh.authorizedKeys.keys = [
-      # Add your public SSH keys here if needed
-    ];
   };
 
   # Optionally, disable root login via SSH for security
   services.openssh = {
     enable = true;
-    settings.permitRootLogin = "no";
+    settings.PermitRootLogin = "no";
   };
 
     programs.zsh.enable = true;

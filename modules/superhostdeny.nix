b020604f13d5b1d540492832abfd967fd6ba382a
JacobWarners <jacob.warners@numbusnetworking.net>
2024-09-21 10:18:56 -0700

    2024-09-21 10:18:56

---
+++ modules/superhostdeny.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+let
+  superhostsDeny = pkgs.fetchurl {
+    url = "https://hosts.ubuntu101.co.za/superhosts.deny;
+    sha256 = "00kdmyz6mgf24c4sd1ifdyijdgl0rqvg1xgzsc0000wkyckrh95m"; # placeholder
+  };
+in
+{
+  environment.etc."hosts.deny".source = superhostsDeny;
+}
+

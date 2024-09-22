0efa1b378487bb508f85392cc8b144ddd035f5e9
JacobWarners <jacob.warners@numbusnetworking.net>
2024-09-22 10:32:56 -0700

    2024-09-22 10:32:56

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -7,7 +7,7 @@
     modesetting.enable = true;
     prime = {
       offload.enable = true;            # Enable offload mode
-      enableOffloadCmd = true;          # Provides the nvidia-offload command for easier offloading
+      offload.enableOffloadCmd = true;          # Provides the nvidia-offload command for easier offloading
       nvidiaBusId = "PCI:130:0:0";      # Replace with your NVIDIA GPU Bus ID
       intelBusId = "PCI:0:2:0";         # Replace with your Intel GPU Bus ID
     };

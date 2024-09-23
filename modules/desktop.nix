5f69a83bfea368e5bf1079260d0a38c770d2b7e3
JacobWarners <jacob.warners@numbusnetworking.net>
2024-09-23 13:07:18 -0700

    2024-09-23 13:07:18

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -5,8 +5,8 @@
 
   # Configure GDM and GNOME
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+#  services.displayManager.sddm.wayland.enable = true;
+#  services.xserver.desktopManager.gnome.enable = true;
 
 
 

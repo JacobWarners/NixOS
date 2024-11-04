a267cd2e3565625d8bc58003da497707d1476c7d
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-03 22:25:53 -0800

    2024-11-03 22:25:53

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -6,21 +6,21 @@
   desktopManager.gnome.enable = true;
 #  displayManager.gdm.wayland = false;
 #  displayManager.sddm.enable = true;
-#   deviceSection = ''
-#     Section "Device"
-#       Identifier "Intel Graphics"
-#       Driver "modesetting"
-#       BusID "PCI:0:2:0"
-#       Option "AccelMethod" "glamor"
-#     EndSection
+   deviceSection = ''
+     Section "Device"
+       Identifier "Intel Graphics"
+       Driver "modesetting"
+       BusID "PCI:0:2:0"
+       Option "AccelMethod" "glamor"
+     EndSection
 
-#     Section "Device"
-#       Identifier "Nvidia Graphics"
-#       Driver "nvidia"
-#       BusID "PCI:130:0:0"
-#       Option "AllowEmptyInitialConfiguration"
-#     EndSection
-#   '';
+     Section "Device"
+       Identifier "Nvidia Graphics"
+       Driver "nvidia"
+       BusID "PCI:130:0:0"
+       Option "AllowEmptyInitialConfiguration"
+     EndSection
+   '';
 
 
    enable = true;
--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -47,7 +47,7 @@
     hardware.nvidia.prime = {
       offload.enableOffloadCmd = true;
       offload.enable = true;
-      nvidiaBusId = "PCI:82:0:0";      # Replace with your NVIDIA GPU Bus ID
+      nvidiaBusId = "PCI:130:0:0";      # Replace with your NVIDIA GPU Bus ID
       intelBusId = "PCI:0:2:0";         # Replace with your Intel GPU Bus ID
     };
 

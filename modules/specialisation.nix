5d3bdd12e6390360be8998110bf1613775dfc56a
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-03 23:12:10 -0800

    2024-11-03 23:12:10

--- modules/specialisation.nix
+++ modules/specialisation.nix
@@ -18,23 +18,20 @@ hardware.opengl = {
     enable = true;
   };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
   hardware.nvidia = {
 
     # Modesetting is required.
-    modesetting.enable = true;
+ #   modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+#    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+#    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -43,11 +40,11 @@ hardware.opengl = {
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+#    open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+#    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
 };

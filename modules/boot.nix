564200caf8e9921c9aa1444c1dffd3b8037555b6
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-03 20:49:35 -0800

    2024-11-03 20:49:35

--- modules/boot.nix
+++ modules/boot.nix
@@ -10,11 +10,11 @@
 
     initrd.luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
       device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
- 
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
-    kernelParams = [ 
-      "nvidia-drm.modeset=1"
-      "modprobe.blacklist=nouveau"
+#    kernelParams = [ 
+#      "nvidia-drm.modeset=1"
+#      "modprobe.blacklist=nouveau"
 ];
       # Blacklist nouveau driver
   };

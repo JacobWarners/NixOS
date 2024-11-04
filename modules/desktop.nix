3d5116291e6ea4d82dfe450d22f780c352a7e8c2
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-03 23:29:14 -0800

    2024-11-03 23:29:14

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -6,21 +6,15 @@
   desktopManager.gnome.enable = true;
 #  displayManager.gdm.wayland = false;
 #  displayManager.sddm.enable = true;
-#  deviceSection = ''
-#    Section "Device"
-#      Identifier "Intel Graphics"
-#      Driver "modesetting"
-#      BusID "PCI:0:2:0"
-#      Option "AccelMethod" "glamor"
-#    EndSection
-
-#    Section "Device"
-#      Identifier "Nvidia Graphics"
-#      Driver "nvidia"
-#      BusID "PCI:130:0:0"
-#      Option "AllowEmptyInitialConfiguration"
-#    EndSection
-#  '';
+  deviceSection = ''
+    Section "Device"
+      Identifier "Nvidia Graphics"
+     Driver "nvidia"
+      BusID "PCI:130:0:0"
+      Option "AllowEmptyInitialConfiguration"
+      Option "AllowExternalGpus" "True"
+    EndSection
+  '';
 
 
    enable = true;
@@ -29,4 +23,12 @@
 
   # Configure GDM and GNOME
 }
+
+#    Section "Device"
+#      Identifier "Intel Graphics"
+#      Driver "modesetting"
+#      BusID "PCI:0:2:0"
+#      Option "AccelMethod" "glamor"
+#    EndSection
+
 

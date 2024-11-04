f24f5de9414c996a62abe9cd683ef6f438f5b814
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-03 20:35:31 -0800

    2024-11-03 20:35:31

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -46,7 +46,7 @@
 
     hardware.nvidia.prime = {
       sync.enable = true;
-      nvidiaBusId = "PCI:130:0:0";      # Replace with your NVIDIA GPU Bus ID
+      nvidiaBusId = "PCI:82:0:0";      # Replace with your NVIDIA GPU Bus ID
       intelBusId = "PCI:0:2:0";         # Replace with your Intel GPU Bus ID
     };
 

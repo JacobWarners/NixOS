9627066533edefa90f20b9ec98dbf658fd085dd5
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-05 19:30:25 +0000

    2024-11-05 19:30:25

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,13 +8,14 @@
       efi.canTouchEfiVariables = true;
     };
 
-    boot.extraModulePackages = [ pkgs.linuxPackages.nvidia_x11];
-    packages = [ pkgs.linuxPackages.nvidia_x11 ];
+    extraModulePackages = [ pkgs.linuxPackages.nvidia_x11];
 
 
     initrd.luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
       device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
     };
+
+    packages = [ pkgs.linuxPackages.nvidia_x11 ];
 #    kernelParams = [ 
 #      "nvidia-drm.modeset=1"
 #      "modprobe.blacklist=nouveau"

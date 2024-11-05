fb71a9a23fec488ad9eceb51ece5342637bbe639
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-04 18:37:06 -0800

    2024-11-04 18:37:06

--- modules/specialisation.nix
+++ modules/specialisation.nix
@@ -50,7 +50,7 @@ in
   hardware.nvidia = {
 
    # Modesetting is required.
-   modesetting.enable = true;
+#   modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking

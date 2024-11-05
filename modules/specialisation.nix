4d4355084f06d045bb164077c0b260273dc046cb
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-05 14:47:29 -0800

    2024-11-05 14:47:29

--- modules/specialisation.nix
+++ modules/specialisation.nix
@@ -8,27 +8,27 @@ in
 
 
 
- environment.etc."X11/xorg.conf.d/11-nvidia.conf".text = ''
-   Section "Device"
-    Identifier "Device0"
-    Driver "nvidia"
-    BusID "PCI:130:0:0"
-    Option "AllowEmptyInitialConfiguration" "true"
-    Option "AllowExternalGpus" "true"
-    Option "PrimaryGPU" "true"
- EndSection
+#environment.etc."X11/xorg.conf.d/11-nvidia.conf".text = ''
+#  Section "Device"
+#   Identifier "Device0"
+#   Driver "nvidia"
+#   BusID "PCI:130:0:0"
+#   Option "AllowEmptyInitialConfiguration" "true"
+#   Option "AllowExternalGpus" "true"
+#   Option "PrimaryGPU" "true"
+#EndSection
 
-   Section "Screen"
-    Identifier "Screen0"
-    Device "Device0"
-    Monitor "Monitor0"
- EndSection
+#  Section "Screen"
+#   Identifier "Screen0"
+#   Device "Device0"
+#   Monitor "Monitor0"
+#EndSection
 
-   Section "Monitor"
-   Identifier "Monitor0"
-   Modeline "2560x1440_180.00"  735.75  2560 2760 3048 3536  1440 1443 1448 1530 -hsync +vsync
- EndSection
-'';
+#  Section "Monitor"
+#  Identifier "Monitor0"
+#  Modeline "2560x1440_180.00"  735.75  2560 2760 3048 3536  1440 1443 1448 1530 -hsync +vsync
+#EndSection
+#'';
 
 
 
@@ -51,13 +51,18 @@ in
 
    # Modesetting is required.
    modesetting.enable = true;
+powerManagement.enable = false;
     open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-#    nvidiaSettings = true;
+nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    nvidiaPersistenced = true;
+#    nvidiaPersistenced = true;
+      prime = {
+        sync.enable = true;
+        allowExternalGpu = true;
+
+        # Make sure to use the correct Bus ID values for your system!
+        nvidiaBusId = "PCI:130:0:0";
+        intelBusId = "PCI:0:2:0";
 
 };
 

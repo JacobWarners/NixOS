186982f6be674e89348c789d536f78fa9f616b79
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-05 15:10:39 -0800

    2024-11-05 15:10:39

--- modules/specialisation.nix
+++ modules/specialisation.nix
@@ -38,11 +38,11 @@ in
     egpu.configuration = {
         system.nixos.tags = ["nvidia"];
 
-        boot = {
+#        boot = {
           # Optionally blacklist the integrated graphics module
-          blacklistedKernelModules = [ "i915" ];
-          kernelParams = [ "module_blacklist=i915" ];
-        };
+#          blacklistedKernelModules = [ "i915" ];
+#          kernelParams = [ "module_blacklist=i915" ];
+#        };
   services.xserver.videoDrivers = ["nvidia"];
 };
 };

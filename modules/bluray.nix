f0c1b292fcee2a932de4076448270b1f09b81e29
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-30 14:31:22 -0800

    bluray6-Nov-30-2024

--- modules/bluray.nix
+++ modules/bluray.nix
@@ -1,25 +1,23 @@
 { config, pkgs, ... }:
 
+{
+  environment.systemPackages = with pkgs; [
+    (pkgs.vlc.override {
+      libbluray = pkgs.libbluray;
+    })
+    libaacs
+    makemkv
+  ];
 
-vlc = pkgs.vlc.override { inherit libbluray; };
+  # Systemd service for AACS keys
+  systemd.services.downloadAACSKeys = {
+    description = "Download AACS KeyDB.cfg for Blu-ray playback";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.curl}/bin/curl -o /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
+      ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown ${config.users.users.jake.name}:${config.users.users.jake.name} /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg && chmod 600 /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg'";
+    };
+  };
+}
 
- {
-   environment.systemPackages = with pkgs; [
-     vlc        # VLC Media Player
-     libbluray  # Library for Blu-ray playback
-     libaacs    # Library for handling AACS DRM
-     makemkv    # Optional: For advanced Blu-ray decryption
-   ];
- 
-   # Add a script to download the AACS keys (KEYDB.cfg) automatically
-   systemd.services.downloadAACSKeys = {
-     description = "Download AACS KeyDB.cfg for Blu-ray playback";
-     wantedBy = [ "multi-user.target" ];
-     serviceConfig = {
-       Type = "oneshot";
-       ExecStart = "${pkgs.curl}/bin/curl -o /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
-       ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown ${config.users.users.jake.name}:${config.users.users.jake.name} /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg && chmod 600 /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg'";
-     };
-   };
- }
- 

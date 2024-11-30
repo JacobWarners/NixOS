159b9893e3d96fc843adf61f73f5645cae87a693
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-30 15:17:19 -0800

    bluray10-Nov-30-2024

--- modules/bluray.nix
+++ modules/bluray.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     # Wrapper script for VLC
     (pkgs.writeShellScriptBin "vlc" ''
       #!${pkgs.bash}/bin/bash
-      export LD_LIBRARY_PATH=${pkgs.libaacs}/lib:${pkgs.libbluray}/lib${lib.makeSearchPath "lib" pkgs.stdenv.cc.cc.libc.out}/lib:$LD_LIBRARY_PATH
+      export LD_LIBRARY_PATH=${pkgs.libaacs}/lib:${pkgs.libbluray}/lib:$LD_LIBRARY_PATH
       exec ${pkgs.vlc}/bin/vlc "$@"
     '')
     makemkv
@@ -17,8 +17,12 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.curl}/bin/curl -o /home/jake/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
-      ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown jake:jake /home/jake/.config/aacs/KEYDB.cfg && chmod 600 /home/jake/.config/aacs/KEYDB.cfg'";
+      ExecStart = ''
+        ${pkgs.curl}/bin/curl -o /home/jake/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg
+      '';
+      ExecStartPost = ''
+        ${pkgs.bash}/bin/bash -c 'chown jake:jake /home/jake/.config/aacs/KEYDB.cfg && chmod 600 /home/jake/.config/aacs/KEYDB.cfg'
+      '';
     };
   };
 }

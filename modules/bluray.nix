369a5c10fe1692518c5d9d2e5309a0a1e6caee23
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-30 14:40:15 -0800

    blueray8-Nov-30-2024

--- modules/bluray.nix
+++ modules/bluray.nix
@@ -1,23 +1,22 @@
 { config, pkgs, ... }:
 
 {
+  # Install the necessary packages
   environment.systemPackages = with pkgs; [
-    (pkgs.vlc.override {
-      libbluray = pkgs.libbluray;
-      libaacs = pkgs.libaacs;
-    })
-#    libaacs
-    makemkv
+    vlc        # VLC media player
+    libbluray  # Library for Blu-ray support
+    libaacs    # Library for AACS decryption
+    makemkv    # MakeMKV for advanced Blu-ray decryption
   ];
 
-  # Systemd service for AACS keys
+  # Systemd service to download AACS keys
   systemd.services.downloadAACSKeys = {
     description = "Download AACS KeyDB.cfg for Blu-ray playback";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.curl}/bin/curl -o /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
-      ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown ${config.users.users.jake.name}:${config.users.users.jake.name} /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg && chmod 600 /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg'";
+      ExecStart = "${pkgs.curl}/bin/curl -o /home/jake/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
+      ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown jake:jake /home/jake/.config/aacs/KEYDB.cfg && chmod 600 /home/jake/.config/aacs/KEYDB.cfg'";
     };
   };
 }

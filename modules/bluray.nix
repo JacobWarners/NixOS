93d24ff3975bdc2c24a8fbb69b3fac0a280ac1e9
JacobWarners <jacob.warners@numbusnetworking.net>
2024-11-30 14:30:06 -0800

    bluray5-Nov-30-2024

--- modules/bluray.nix
+++ modules/bluray.nix
@@ -1,24 +1,25 @@
 { config, pkgs, ... }:
-{
+
+
 vlc = pkgs.vlc.override { inherit libbluray; };
-}
-# {
-#   environment.systemPackages = with pkgs; [
-#     vlc        # VLC Media Player
-#     libbluray  # Library for Blu-ray playback
-#     libaacs    # Library for handling AACS DRM
-#     makemkv    # Optional: For advanced Blu-ray decryption
-#   ];
-# 
-#   # Add a script to download the AACS keys (KEYDB.cfg) automatically
-#   systemd.services.downloadAACSKeys = {
-#     description = "Download AACS KeyDB.cfg for Blu-ray playback";
-#     wantedBy = [ "multi-user.target" ];
-#     serviceConfig = {
-#       Type = "oneshot";
-#       ExecStart = "${pkgs.curl}/bin/curl -o /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
-#       ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown ${config.users.users.jake.name}:${config.users.users.jake.name} /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg && chmod 600 /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg'";
-#     };
-#   };
-# }
-# 
+
+ {
+   environment.systemPackages = with pkgs; [
+     vlc        # VLC Media Player
+     libbluray  # Library for Blu-ray playback
+     libaacs    # Library for handling AACS DRM
+     makemkv    # Optional: For advanced Blu-ray decryption
+   ];
+ 
+   # Add a script to download the AACS keys (KEYDB.cfg) automatically
+   systemd.services.downloadAACSKeys = {
+     description = "Download AACS KeyDB.cfg for Blu-ray playback";
+     wantedBy = [ "multi-user.target" ];
+     serviceConfig = {
+       Type = "oneshot";
+       ExecStart = "${pkgs.curl}/bin/curl -o /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg https://vlc-bluray.whoknowsmy.name/files/KEYDB.cfg";
+       ExecStartPost = "${pkgs.bash}/bin/bash -c 'chown ${config.users.users.jake.name}:${config.users.users.jake.name} /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg && chmod 600 /home/${config.users.users.jake.name}/.config/aacs/KEYDB.cfg'";
+     };
+   };
+ }
+ 

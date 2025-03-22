9186c70103e73bfe2c043ac179ef09e4b8ea01ef
NanoBlazer915 <jacob.warners@weka.io>
2025-03-22 16:57:26 -0700

    Testpinningchromeandnvidia-Mar-22-2025

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+# configuration.nix
 { config, pkgs, ... }:
 
 {
@@ -22,15 +23,31 @@
     ./modules/docker.nix
   ];
 
-  # Add the overlay to disable etcd tests
   nixpkgs.overlays = [
     (final: prev: {
-      etcd = prev.etcd.overrideAttrs (oldAttrs: {
-        doCheck = false;
-        checkPhase = "echo 'Tests skipped for etcd'";
-      });
+      chromium =
+        (import (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/fd40cef8d797670e203a27a91e4b8e6decf0b90c.tar.gz";
+          sha256 = "1xxmih8zbxk80m6r0zd5qp6z6a4cxl7n1cmnlhs5wi87n4sfz24w"# Replace with real hash
+            }) { system = "x86_64-linux";
+          config.allowUnfree = true;
+        }).chromium;
+          linuxPackages = prev.linuxPackages.extend (lself: lsuper: {
+      nvidia_x11 = (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/fd40cef8d797670e203a27a91e4b8e6decf0b90c.tar.gz";
+      sha256 = "1xxmih8zbxk80m6r0zd5qp6z6a4cxl7n1cmnlhs5wi87n4sfz24w"; # Replace with real hash
+    })
+    { system = "x86_64-linux"; config.allowUnfree = true; }
+    ).linuxPackages.nvidia_x11;
+    });
     })
   ];
+
+  # Ensure VAAPI support for Intel
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl ];
+  };
 
   system.stateVersion = "24.11";
 }

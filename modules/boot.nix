d883d54ca4fbddecf98c79edeb6b66110ca82f49
NanoBlazer915 <jacob.warners@weka.io>
2025-05-12 14:14:48 -0700

    AMDnow-May-12-2025

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,9 +8,11 @@
       systemd-boot.configurationLimit = 5;
     };
 
-    boot.initrd.kernelModules = [ "amdgpu" ];
-    initrd.luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
-      device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
+    initrd = {
+      kernelModules = [ "amdgpu" ];
+      luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
+        device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
+      };
     };
   };
 }

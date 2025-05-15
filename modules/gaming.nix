4ba23cc20987ac136c758486fca84b5a022c5ba0
NanoBlazer915 <jacob.warners@weka.io>
2025-05-15 16:14:43 -0700

    addamdfinal-May-15-2025

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 {
-
   environment.systemPackages = with pkgs; [
     (pkgs.writeShellScriptBin "xivlauncher-amd" ''
       export DXVK_HUD=1
@@ -9,18 +8,9 @@
       export DXVK_FRAME_RATE=0
       export DRI_PRIME=1
       export VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json
-
       exec /nix/store/knl8i4wqf8z448xc1lgqk673bs4gdsb3-XIVLauncher-1.1.2/bin/.XIVLauncher.Core-wrapped "$@"
     '')
-  ];
 
-  # Enable Steam
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
-  services.pulseaudio.support32Bit = true;
-
-  # Install FFXIV launcher (assuming it's available in Nixpkgs)
-  environment.systemPackages = with pkgs; [
     steam
     mesa
     vulkan-loader
@@ -32,5 +22,8 @@
     winetricks
   ];
 
+  # Enable Steam-related support
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  services.pulseaudio.support32Bit = true;
 }
-

6c8ec603fe94fffdac062ae4a4995b3bb896e8c7
NanoBlazer915 <jacob.warners@weka.io>
2025-05-15 16:04:48 -0700

    amdfinalfantasywrap-May-15-2025

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,6 +1,19 @@
 { config, pkgs, ... }:
 
 {
+{
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "xivlauncher-amd" ''
+      export DXVK_HUD=1
+      export DXVK_VSYNC=0
+      export DXVK_FRAME_RATE=0
+      export DRI_PRIME=1
+      export VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json
+
+      exec /nix/store/knl8i4wqf8z448xc1lgqk673bs4gdsb3-XIVLauncher-1.1.2/bin/.XIVLauncher.Core-wrapped "$@"
+    '')
+  ];
+}
   # Enable Steam
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;

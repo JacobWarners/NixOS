683c85f444bb947b258620a5d9cc38430b94eada
NanoBlazer915 <jacob.warners@weka.io>
2025-06-09 13:28:54 -0700

    Zoom-flat-Jun-09-2025

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -3,8 +3,10 @@
 ##This is only for prusa cause too hard
 
 {
+  xdg.mime.defaultApplication = {
+    "x-scheme-handler/zoommtg" = "us.zoom.Zoom.desktop";
   services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-}
+};
 

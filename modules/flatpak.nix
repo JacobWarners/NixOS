859fa23450b41d331d0ffa5a0808ddc4f1cc2280
NanoBlazer915 <jacob.warners@weka.io>
2025-06-09 13:02:32 -0700

    Flatpak-launcher-Jun-09-2025

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -4,7 +4,7 @@
 
 {
     xdg.data.paths = [
-          "/var/lib/flatpak/exports/share";
+          "/var/lib/flatpak/exports/share"
   services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];

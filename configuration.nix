d4de8f8ca6969df4ff5b808ce22b15257fe4a65b
NanoBlazer915 <jacob.warners@weka.io>
2025-06-09 12:49:48 -0700

    Portals-zoom-Jun-09-2025

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,39 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    #    ./modules/specialisation.nix
-    #    ./modules/hotplugegpu.nix
-    ./modules/amd.nix
-    ./modules/flatpak.nix
-    ./modules/nix.nix
-    ./modules/nix-ld.nix
-    ./modules/bluray.nix
-    ./modules/boot.nix
-    ./modules/network.nix
-    ./modules/locale.nix
-    ./modules/desktop.nix
-    #    ./modules/nvidia-egpu.nix
-    ./modules/audio.nix
-    ./modules/users.nix
-    ./modules/unfree.nix
-    ./modules/global-packages.nix
-    ./modules/virtualization.nix
-    ./modules/gaming.nix
-    ./modules/docker.nix
-    # Add any other modules you have
-  ];
+  programs.xdg.portal = {
+    enable = true;
+    # You might want to specify the backend for your desktop environment
+    # For GNOME, Wayfire, Sway, or other GTK-based environments:
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  system.stateVersion = "24.11";
+    imports = [
+      ./hardware-configuration.nix
+      #    ./modules/specialisation.nix
+      #    ./modules/hotplugegpu.nix
+      ./modules/amd.nix
+      ./modules/flatpak.nix
+      ./modules/nix.nix
+      ./modules/nix-ld.nix
+      ./modules/bluray.nix
+      ./modules/boot.nix
+      ./modules/network.nix
+      ./modules/locale.nix
+      ./modules/desktop.nix
+      #    ./modules/nvidia-egpu.nix
+      ./modules/audio.nix
+      ./modules/users.nix
+      ./modules/unfree.nix
+      ./modules/global-packages.nix
+      ./modules/virtualization.nix
+      ./modules/gaming.nix
+      ./modules/docker.nix
+      # Add any other modules you have
+    ];
 
-  # Global settings can be added here if necessary
+    system.stateVersion = "24.11";
 
-}
+    # Global settings can be added here if necessary
+
+  }
 

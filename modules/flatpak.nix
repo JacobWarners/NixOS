1b49a303209574bb2b9289ea9ae8cf9c1a38b67e
NanoBlazer915 <jacob.warners@weka.io>
2025-06-09 13:29:25 -0700

    Zoom-flat-Jun-09-2025

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -5,8 +5,9 @@
 {
   xdg.mime.defaultApplication = {
     "x-scheme-handler/zoommtg" = "us.zoom.Zoom.desktop";
+  };
   services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-};
+}
 

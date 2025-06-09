be855c782a00c01bce18db7e7f4bd7f664ede0db
NanoBlazer915 <jacob.warners@weka.io>
2025-06-09 12:59:03 -0700

    Flatpak-in-data-dir-Jun-09-2025

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -3,6 +3,10 @@
 ##This is only for prusa cause too hard
 
 {
+
+  environment.sessionVariables = {
+        XDG_DATA_DIRS = "$XDG_DATA_DIRS:/var/lib/flatpak/exports/share";
+          };
   services.flatpak.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];

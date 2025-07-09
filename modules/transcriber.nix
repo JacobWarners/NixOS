a5db5fa11b67425adfebe242188ed1d4b5c8a847
NanoBlazer915 <jacob.warners@weka.io>
2025-07-08 17:49:46 -0700

    Python-transcriber-Jul-08-2025

--- modules/transcriber.nix
+++ modules/transcriber.nix
@@ -1,39 +1,25 @@
 # /etc/nixos/configuration.nix
+...
 
-{ config, pkgs, ... }:
+systemd.services.auto-transcriber = {
+  description = "Watches the OBS folder and transcribes new videos";
+  # ... other settings are the same ...
 
-{
-  # ... your other existing configuration ...
-
-  # Define the auto-transcriber background service
-  systemd.services.auto-transcriber = {
-    description = "Watches the OBS folder and transcribes new videos";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-
-    # The main service configuration
-    serviceConfig = {
-      # The user and group the service will run as
-      User = "jake";
-      Group = "users";
-
-      # The directory where your script and shell.nix are located
-      WorkingDirectory = "/home/jake/Videos/OBS/"; # Make sure this path is correct!
-
-      # Set the Gemini API Key securely for the service
-      Environment = "GEMINI_API_KEY=YOUR_GEMINI_API_KEY"; # 👈 Paste your key here
-
-      # For stability, always restart the service if it fails
-      Restart = "always";
-      RestartSec = 10;
-    };
-
-    # The command that the service will run on startup
-    script = ''
-      # We use the full path to nix-shell to be safe
-      ${pkgs.nix}/bin/nix-shell --run "python3 gemini_transcriber.py"
-    '';
+  serviceConfig = {
+    User = "jake";
+    Group = "users";
+    WorkingDirectory = "/home/jake/Videos/OBS/Code";
+    Restart = "always";
+    RestartSec = 10;
+    
+    # This is the new part: Securely load the API key from your .env file
+    LoadCredential = "GEMINI_API_KEY:/home/jake/.config/auto-transcriber.env";
   };
 
-  # ... rest of your configuration ...
-}
+  script = ''
+    # The python script will now get the key from the environment
+    ${pkgs.nix}/bin/nix-shell --run "python3 gemini_transcriber.py"
+  '';
+};
+
+...

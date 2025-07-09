8b87bbc9e425d3edf6c5cbe384889725644fb0d4
NanoBlazer915 <jacob.warners@weka.io>
2025-07-08 17:51:25 -0700

    transcriber-nix-Jul-08-2025

--- modules/transcriber.nix
+++ modules/transcriber.nix
@@ -1,21 +1,28 @@
-systemd.services.auto-transcriber = {
-  description = "Watches the OBS folder and transcribes new videos";
-  # ... other settings are the same ...
+# ./modules/transcriber.nix
 
-  serviceConfig = {
-    User = "jake";
-    Group = "users";
-    WorkingDirectory = "/home/jake/Videos/OBS/Code";
-    Restart = "always";
-    RestartSec = 10;
-    
-    # This is the new part: Securely load the API key from your .env file
-    LoadCredential = "GEMINI_API_KEY:/home/jake/.config/auto-transcriber.env";
+{ config, pkgs, ... }:
+
+{
+  # This line is the start of the configuration set
+  
+  systemd.services.auto-transcriber = {
+    description = "Watches the OBS folder and transcribes new videos";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      User = "jake";
+      Group = "users";
+      WorkingDirectory = "/home/jake/Videos/OBS/Code";
+      Restart = "always";
+      RestartSec = 10;
+      LoadCredential = "GEMINI_API_KEY:/home/jake/.config/auto-transcriber.env";
+    };
+
+    script = ''
+      ${pkgs.nix}/bin/nix-shell --run "python3 gemini_transcriber.py"
+    '';
   };
 
-  script = ''
-    # The python script will now get the key from the environment
-    ${pkgs.nix}/bin/nix-shell --run "python3 gemini_transcriber.py"
-  '';
-};
-
+  # This line closes the configuration set
+}

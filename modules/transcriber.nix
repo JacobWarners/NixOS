f28a72de5fcae0797fc3d7d9fdeca456280e1f1e
NanoBlazer915 <jacob.warners@weka.io>
2025-07-08 17:59:02 -0700

    py-tran-Jul-08-2025

--- modules/transcriber.nix
+++ modules/transcriber.nix
@@ -22,6 +22,6 @@
     # This command will print the API key to the log if it exists, or an error if it doesn't.
     ${pkgs.nix}/bin/nix-shell --run "python3 -c 'import os; print(\"API Key found: \" + os.getenv(\"GEMINI_API_KEY\", \"KEY NOT FOUND\"))'"
   '';
-
+};
 
 }

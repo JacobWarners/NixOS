e0a7f5f13e8cd0f7cc2f551ee9de46e4eadc80a5
NanoBlazer915 <jacob.warners@weka.io>
2025-07-08 18:00:26 -0700

    python-transcriber-Jul-08-2025

--- modules/transcriber.nix
+++ modules/transcriber.nix
@@ -1,11 +1,9 @@
 # ./modules/transcriber.nix
-
 { config, pkgs, ... }:
 
 {
-  # This line is the start of the configuration set
-  
   systemd.services.python-transcriber = {
+    # ... description, wantedBy, after, serviceConfig are all the same ...
     description = "Watches the OBS folder and transcribes new videos";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
@@ -18,10 +16,15 @@
       RestartSec = 10;
       EnvironmentFile = "/home/jake/.config/python-transcriber.env";
     };
-  script = ''
-    # This command will print the API key to the log if it exists, or an error if it doesn't.
-    ${pkgs.nix}/bin/nix-shell --run "python3 -c 'import os; print(\"API Key found: \" + os.getenv(\"GEMINI_API_KEY\", \"KEY NOT FOUND\"))'"
-  '';
-};
 
+    # This updated script tells nix-shell where to find the nix packages
+    script = ''
+      set -a
+      source /home/jake/.config/python-transcriber.env
+      set +a
+      
+      # The -I flag explicitly tells nix-shell where your system's packages are
+      ${pkgs.nix}/bin/nix-shell -I nixpkgs=${pkgs.path} --run "python3 gemini_transcriber.py"
+    '';
+  };
 }

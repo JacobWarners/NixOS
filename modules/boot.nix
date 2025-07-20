2b697d2e31477f16458c66af7aab1e1d3b191816
NanoBlazer915 <jacob.warners@weka.io>
2025-07-19 22:47:18 -0700

    sdf-Jul-19-2025

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,49 +1,43 @@
 { config, pkgs, ... }:
 
 let
-  # A simplified test derivation
-  myCustomPlymouthTheme = pkgs.stdenv.mkDerivation {
-    pname = "abstract-ring-alt";
-    version = "1.0";
-
-    # We remove the 'src' and just create an empty directory and a test file.
-    installPhase = ''
-      mkdir -p $out/share/plymouth/themes/${pname}
-      touch $out/share/plymouth/themes/${pname}/test-file
-    '';
-  };
+  # This uses a different, simpler builder as a test
+  myCustomPlymouthTheme = pkgs.runCommand "abstract-ring-alt-plymouth-theme" { } ''
+    # Here, we hard-code the theme name instead of using a variable
+    mkdir -p $out/share/plymouth/themes/abstract-ring-alt
+    cp -r ${../plymouth-themes/abstract_ring_alt}/* $out/share/plymouth/themes/abstract-ring-alt/
+  '';
 in
 {
-  boot = {
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
-      "amdgpu.modeset=1"
-    ];
+  boot.plymouth = {
+    enable = true;
+    theme = "abstract-ring-alt";
+    themePackages = [ myCustomPlymouthTheme ];
+  };
 
-    plymouth = {
-      enable = true;
-      theme = "abstract-ring-alt";
-      themePackages = [ myCustomPlymouthTheme ];
-    };
+  # The rest of your boot config...
+  consoleLogLevel = 0;
+  initrd.verbose = false;
+  kernelParams = [
+    "quiet"
+    "splash"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+    "amdgpu.modeset=1"
+  ];
 
-    kernelPackages = pkgs.linuxPackages_latest;
-    supportedFilesystems = [ "nfs" ];
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      systemd-boot.configurationLimit = 5;
-    };
-    initrd = {
-      systemd.enable = true;
-      kernelModules = [ "amdgpu" ];
-      luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
-        device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
-      };
+  kernelPackages = pkgs.linuxPackages_latest;
+  supportedFilesystems = [ "nfs" ];
+  loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 5;
+  };
+  initrd = {
+    systemd.enable = true;
+    kernelModules = [ "amdgpu" ];
+    luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
+      device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
     };
   };
 }

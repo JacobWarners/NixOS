3b0d4abce952e8e767fd18694e67a0b4fbd6826c
NanoBlazer915 <jacob.warners@weka.io>
2025-07-19 20:52:09 -0700

    ratat-beeupdate-Jul-19-2025

--- home/dotfiles/dots/waybar/scripts/nyt-games.sh
+++ home/dotfiles/dots/waybar/scripts/nyt-games.sh
@@ -3,13 +3,15 @@
 SPELLING_BEE_URL="https://www.nytimes.com/puzzles/spelling-bee"
 BUDDY_URL="https://www.nytimes.com/interactive/2023/upshot/spelling-bee-buddy.html"
 
+pactl set-sink-volume @DEFAULT_SINK@ 100%
+pactl set-sink-mute @DEFAULT_SINK@ 0
 hyprctl dispatch workspace 9
 chromium --new-window &
 paplay ~/.config/waybar/sounds/spellingbee.wav
-sleep 2
+sleep 1
 hyprctl dispatch workspace 5
 firefox --new-window "$SPELLING_BEE_URL" &
-sleep 2
+sleep 1
 firefox --new-window "$BUDDY_URL" &
 hyprctl dispatch togglefloating active
 hyprctl dispatch fullscreen 1

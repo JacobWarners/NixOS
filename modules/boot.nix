dbea1d388b46458d08f77c513a7de1defe4b8f7a
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 12:18:25 -0700

    ekw-Jul-20-2025

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,33 +7,32 @@
 
     # Add this line to force early loading for Plymouth
     initrd.availableKernelModules = [ "amdgpu" ];
+    # silence first boot output
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    initrd.systemd.enable = true;
+    kernelParams = [
+        "quiet"
+        "splash"
+        "intremap=on"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+    ];
 
-    plymouth = {
-      enable = true;
-      theme = "abstract-ring-alt";
-      themePackages = [
-        (pkgs.stdenv.mkDerivation {
-          name = "my-custom-plymouth-theme";
-          src = inputs.self + "/plymouth-themes/abstract-ring-alt";
-          installPhase = ''
-            mkdir -p $out/share/plymouth/themes/abstract-ring-alt
-            cp -R ./* $out/share/plymouth/themes/abstract-ring-alt/
-          '';
-        })
-      ];
+    # plymouth, showing after LUKS unlock
+    plymouth.enable = true;
+    plymouth.font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
+    plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+};
+}
+
     };
 
     # Restore the boot entry limit
     loader.systemd-boot.configurationLimit = 3;
 
     # Other boot options
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "rd.systemd.show_status=auto"
-    ];
 
     kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;

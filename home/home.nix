915092b2d46a4657d1ee1c0bc0e4e68d9660d399
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 12:09:01 -0700

    we-Jul-20-2025

--- home/home.nix
+++ home/home.nix
@@ -110,12 +110,6 @@ in {
       enable = true;
       platformTheme.name = "gtk";
   };
-  home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 20;
-  };
   gtk = {
     enable = true;
     iconTheme = {

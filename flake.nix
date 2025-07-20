c82b435704adb9d469522f40f9d76dc4605b60dd
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 10:15:08 -0700

    fwe-Jul-20-2025

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
           ./configuration.nix
 
           # Home Manager module setup
-          home-manager.nixosModules.home-manager,
+          home-manager.nixosModules.home-manager;
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;

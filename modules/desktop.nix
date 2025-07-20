ac437876dade508bffb0cebac242f139c6905f3c
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 13:12:50 -0700

    test-Jul-20-2025

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -9,13 +9,10 @@
   };
 
   # 2. Configure LightDM with Auto-Login 🚀
-  services.xserver.displayManager.sddm = {
+  services.displayManager.sddm = {
     enable = true;
     theme = "sddm-astronaut";
     # Auto-login the specified user.
     # Set a timeout for auto-login, or set to 0 to log in immediately.
   };
-  # 5. Disable greetd
-  # This ensures your old display manager does not conflict with the new one.
-  services.greetd.enable = false;
 }

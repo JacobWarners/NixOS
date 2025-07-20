22a2fe036091747305514120bbedf6509873a027
NanoBlazer915 <jacob.warners@weka.io>
2025-07-19 19:47:58 -0700

    dsf-Jul-19-2025

--- apps/ratatat-rust/flake.nix
+++ apps/ratatat-rust/flake.nix
@@ -1,61 +1,44 @@
 # /path/to/your/apps/ratatat-rust/flake.nix
-
 {
   description = "A NixOS daemon to listen for the 'ratatat' key sequence.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      pkgsFor = system: import nixpkgs { inherit system; };
-    in
-    {
-      # This devShell is what `nix develop` uses.
-      # It provides an environment with all the necessary build tools.
-      devShells.x86_64-linux.default =
-        let
-          pkgs = pkgsFor "x86_64-linux";
-        in
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.rustc
-            pkgs.cargo
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # Define the package at the top level. This is a robust pattern.
+        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "ratatat-listener";
+          version = "0.1.0";
+          src = self;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          # Provide all necessary dependencies for inputbot, including X11.
+          nativeBuildInputs = [
             pkgs.pkg-config
-            pkgs.wayland
-            pkgs.libxkbcommon
+            pkgs.udev
+            pkgs.xorg.xlibsWrapper
           ];
         };
 
-      # Define the NixOS module.
-      nixosModules.default = { config, lib, pkgs, ... }:
-        let
-          system = pkgs.system;
-          localPkgs = pkgsFor system;
-          ratatat-pkg = localPkgs.rustPlatform.buildRustPackage rec {
-            pname = "ratatat-listener";
-            version = "0.1.0";
-            src = self;
-
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-
-            # With the Cargo.toml change, we only need Wayland-related dependencies.
-            nativeBuildInputs = [
-              localPkgs.pkg-config
-              localPkgs.wayland
-              localPkgs.libxkbcommon
-            ];
-          };
-        in
-        {
+        # Define the NixOS module that will use the package.
+        nixosModules.default = { config, lib, ... }: {
           options.services.ratatat-listener.enable = lib.mkEnableOption "Enable the ratatat listener daemon";
 
           config = lib.mkIf config.services.ratatat-listener.enable {
+            # Install the final package (built above) and mpg123.
             environment.systemPackages = [
-              ratatat-pkg
+              self.packages.${system}.default
               pkgs.mpg123
             ];
 
@@ -66,12 +49,26 @@
               partOf = [ "graphical-session.target" ];
               serviceConfig = {
                 User = "jake";
-                ExecStart = "${ratatat-pkg}/bin/ratatat-listener";
+                # The ExecStart now points to the top-level package.
+                ExecStart = "${self.packages.${system}.default}/bin/ratatat-listener";
                 Restart = "always";
                 RestartSec = "5s";
               };
             };
           };
         };
-    };
+
+        # Define the devShell for `nix develop`.
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.pkg-config
+            pkgs.udev
+            pkgs.xorg.xlibsWrapper
+          ];
+        };
+      }
+    );
 }
+

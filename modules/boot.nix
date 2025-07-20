369e1304f8c3b1280cbd424b4cd4eb7dc81a1557
NanoBlazer915 <jacob.warners@weka.io>
2025-07-19 22:48:22 -0700

    tfy-Jul-19-2025

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  # This uses a different, simpler builder as a test
+  # This uses a different, simpler builder which is proven to work in your setup
   myCustomPlymouthTheme = pkgs.runCommand "abstract-ring-alt-plymouth-theme" { } ''
     # Here, we hard-code the theme name instead of using a variable
     mkdir -p $out/share/plymouth/themes/abstract-ring-alt
@@ -9,35 +9,37 @@ let
   '';
 in
 {
-  boot.plymouth = {
-    enable = true;
-    theme = "abstract-ring-alt";
-    themePackages = [ myCustomPlymouthTheme ];
-  };
+  # All boot options MUST be inside this block
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "abstract-ring-alt";
+      themePackages = [ myCustomPlymouthTheme ];
+    };
 
-  # The rest of your boot config...
-  consoleLogLevel = 0;
-  initrd.verbose = false;
-  kernelParams = [
-    "quiet"
-    "splash"
-    "udev.log_priority=3"
-    "rd.systemd.show_status=auto"
-    "amdgpu.modeset=1"
-  ];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+      "amdgpu.modeset=1"
+    ];
 
-  kernelPackages = pkgs.linuxPackages_latest;
-  supportedFilesystems = [ "nfs" ];
-  loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-    systemd-boot.configurationLimit = 5;
-  };
-  initrd = {
-    systemd.enable = true;
-    kernelModules = [ "amdgpu" ];
-    luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
-      device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = [ "nfs" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 5;
+    };
+    initrd = {
+      systemd.enable = true;
+      kernelModules = [ "amdgpu" ];
+      luks.devices."luks-6cb1713b-252a-435f-8c5c-d4b404e9db96" = {
+        device = "/dev/disk/by-uuid/6cb1713b-252a-435f-8c5c-d4b404e9db96";
+      };
     };
   };
 }

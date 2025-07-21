150e3de2abb87ed660d1c11fa1bef1420e45e051
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 22:28:59 -0700

    nocolor-Jul-20-2025

--- home/dotfiles/dots/waybar/w2.sh
+++ home/dotfiles/dots/waybar/w2.sh
@@ -1,38 +1,20 @@
 #!/usr/bin/env sh
-
-# --- Define Your Colors ---
-COLOR_ACTIVE='blue'
-COLOR_INACTIVE='green'
-COLOR_EMPTY='yellow'
-
-# --- Define Your Icons ---
-ICON_ACTIVE='A'
-ICON_INACTIVE='C'
-ICON_EMPTY='O'
-
-# 1. Get the active workspace ID
-ACTIVE_WORKSPACE=$(hyprctl monitors | grep 'focused: yes' -B 1 | head -n 1 | awk '{print $2}')
-
-# 2. Build the output string
-FINAL_OUTPUT=""
-for I in $(seq 1 10); do # Loop from 1 to 10
-    ICON=$ICON_EMPTY
-    COLOR=$COLOR_EMPTY
-
-    # Check if the workspace is active
-    if [ "$I" -eq "$ACTIVE_WORKSPACE" ]; then
-        ICON=$ICON_ACTIVE
-        COLOR=$COLOR_ACTIVE
-    # Check if the workspace exists but is not active
-    elif hyprctl workspaces | grep -q "ID $I on"; then
-        ICON=$ICON_INACTIVE
-        COLOR=$COLOR_INACTIVE
-    fi
-
-    # Append the formatted character to the final output string
-    # Using printf for better formatting control
-    FINAL_OUTPUT="${FINAL_OUTPUT}$(printf "<span color='%s'>%s</span> " "$COLOR" "$ICON")"
+A=$(hyprctl monitors | grep workspace| head -n 1 | awk '{print $3}')
+L=1
+for i in $(hyprctl workspaces | grep "workspace ID" | grep -v lastwindowtitle | cut -d ' ' -f 3 | sort -g)
+do
+	#echo $i $L
+	while [ $L -le $i ]
+	do
+		if [ ! $L == $i ]; then
+			echo -n " O   "
+		fi
+		L=$(( $L + 1 ))
+	done
+	if [ $A == $i ]; then
+		echo -n " A   "
+	else
+		echo -n " C   "
+	fi
 done
-
-# 3. Echo the final, fully-formatted string once
-echo "${FINAL_OUTPUT}"
+echo ""

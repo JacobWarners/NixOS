78256cb738403b9ca31b334ef9ad5019d6c3358d
NanoBlazer915 <jacob.warners@weka.io>
2025-07-20 22:11:28 -0700

    FASTER-Jul-20-2025

--- home/dotfiles/dots/waybar/w2.sh
+++ home/dotfiles/dots/waybar/w2.sh
@@ -7,14 +7,14 @@ do
 	while [ $L -le $i ]
 	do
 		if [ ! $L == $i ]; then
-			echo -n "  "
+			echo -n "   "
 		fi
 		L=$(( $L + 1 ))
 	done
 	if [ $A == $i ]; then
-		echo -n "A    "
+		echo -n " A    "
 	else
-		echo -n "C    "
+		echo -n " C    "
 	fi
 done
 echo ""

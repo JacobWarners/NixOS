34dfc2c62d2e6f745b17d7c72424569c144783df
NanoBlazer915 <jacob.warners@weka.io>
2025-07-22 11:35:49 -0700

    wkje-Jul-22-2025

--- home/dotfiles/dots/waybar/w2.sh
+++ home/dotfiles/dots/waybar/w2.sh
@@ -4,45 +4,44 @@
 COLOR_ACTIVE='#03d7fc'
 COLOR_INACTIVE='#03fc45'
 COLOR_EMPTY='#e6e600'
-COLOR_SUPER_CHARGE='#fcf403' # The yellow from your super-sonic animation
+COLOR_SUPER_CHARGE='#fcf403'
 
 # --- Get Battery Level ---
-# Note: Your battery might be BAT1 or something else.
-# Check your system with the command: ls /sys/class/power_supply/
+# Note: Your battery might be BAT1. Check with: ls /sys/class/power_supply/
 BATT_PATH="/sys/class/power_supply/BAT0"
 BATT_LEVEL=0 # Default to 0 if battery not found
 if [ -f "${BATT_PATH}/capacity" ]; then
     BATT_LEVEL=$(cat "${BATT_PATH}/capacity")
 fi
 
-# --- Workspace Logic ---
+# --- Prepare variables ---
 A=$(hyprctl monitors | grep workspace | head -n 1 | awk '{print $3}')
 L=1
+output_text=""
+css_class="" # This will hold our dynamic class name
 
+# --- Build the workspace string ---
 for i in $(hyprctl workspaces | grep "workspace ID" | grep -v lastwindowtitle | cut -d ' ' -f 3 | sort -g)
 do
-    while [ $L -le $i ]
-    do
+    while [ $L -le $i ]; do
         if [ ! $L == $i ]; then
-            echo -n " <span color='${COLOR_EMPTY}'>o</span>   "
+            output_text="${output_text} <span color='${COLOR_EMPTY}'>o</span>   "
         fi
         L=$(( $L + 1 ))
     done
 
-    # This is the main conditional logic block
     if [ "$A" -eq "$i" ]; then
-        # If workspace is active, check battery level
-        if [ "$BATT_LEVEL" -eq 19 ]; then
-            # AT 100%: Display 'B' with the super-charge flashing animation
-            echo -n " <span class='super-charge-flash' color='${COLOR_SUPER_CHARGE}'>B</span>   "
+        if [ "$BATT_LEVEL" -eq 100 ]; then
+            output_text="${output_text}<span color='${COLOR_SUPER_CHARGE}'>B</span>   "
+            css_class="super-charge-flash" # Set class for full battery
         else
-            # BELOW 100%: Display 'A' with the regular flashing animation
-            echo -n " <span class='flashing' color='${COLOR_ACTIVE}'>A</span>   "
+            output_text="${output_text}<span color='${COLOR_ACTIVE}'>A</span>   "
         fi
     else
-        # Inactive but occupied workspaces
-        echo -n " <span color='${COLOR_INACTIVE}'>C</span>   "
+        output_text="${output_text}<span color='${COLOR_INACTIVE}'>C</span>   "
     fi
 done
 
-echo ""
+# --- Output JSON for Waybar ---
+# This prints the final JSON object that Waybar understands.
+printf '{"text": "%s", "class": "%s"}\n' "$output_text" "$css_class"

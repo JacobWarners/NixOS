1ec6f8b8cbfa9144efd97e088e8dc532e92f8714
NanoBlazer915 <jacob.warners@weka.io>
2025-08-18 12:58:34 -0700

    no way-Aug-18-2025

--- modules/amd.nix
+++ modules/amd.nix
@@ -1,39 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-   hardware.graphics = {
-     enable = true;
-     enable32Bit = true;
- 
-     
-     extraPackages = with pkgs; [
-       driversi686Linux.amdvlk
-     ];
-   }; 
-#    extraPackages32 = with pkgsi686Linux; [
-#        intel-media-driver
-#        intel-vaapi-driver
-#      ];
-hardware.amdgpu.overdrive.enable = true;
-hardware.amdgpu.amdvlk.support32Bit.enable = true;
-hardware.amdgpu.overdrive.ppfeaturemask = "0xffffffff";
-hardware.amdgpu.amdvlk.enable = true;
-programs.gamemode.enable = true;
-#powerManagement.cpuFreqGovernor = "performance";
-  # Optionally, prefer AMDVLK over RADV for better performance in some cases
-#  environment.variables = {
-#    AMD_VULKAN_ICD = "AMDVLK";
-#    VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json:/run/opengl-driver-32/share/vulkan/icd.d/amd_icd32.json";
-#  };
-  
-   # Add kernel parameters for better performance
-# boot.kernelParams = [
-#      "amdgpu.ppfeaturemask=0xffffffff" # Enables power management features
-#      "radeon.si_support=0"
-#      "radeon.cik_support=0"
-#      "amdgpu.si_support=1"
-#      "amdgpu.cik_support=1"
-#      "pci=realloc=on"
-# 
-#   ];
+  # This is the ONLY block you need for your graphics drivers.
+  # It correctly enables the standard Mesa/RADV drivers for BOTH your Intel and AMD GPUs,
+  # including the 32-bit support that Steam absolutely requires.
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # This is for GPU performance tuning. It is safe to keep.
+  hardware.amdgpu.overdrive.enable = true;
+  hardware.amdgpu.overdrive.ppfeaturemask = "0xffffffff";
+
+  # Enable gamemode for better performance in games.
+  programs.gamemode.enable = true;
 }

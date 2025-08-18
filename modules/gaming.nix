0cadeaafb96456ff02916b4b0bf274f7b4a01ee0
NanoBlazer915 <jacob.warners@weka.io>
2025-08-18 15:50:41 -0700

    wekfj-Aug-18-2025

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -2,15 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # ... your other packages ...
-
-    # This script now launches Steam on the STABLE Integrated GPU.
-    # We will tell Steam how to launch games on the AMD GPU separately.
-    (pkgs.writeShellScriptBin "steam-stable" ''
+    (pkgs.writeShellScriptBin "steam" ''
       #!${pkgs.bash}/bin/bash
       export DRI_PRIME=0
       exec ${pkgs.steam}/bin/steam "$@"
     '')
+    # ... your other packages
   ];
 
   environment.variables = {
@@ -20,9 +17,15 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+    
+    # === FINAL MANGO HUD FIX ===
+    # Add both 64-bit and 32-bit MangoHud to ensure it works
+    # with any game, regardless of its architecture.
     extraPackages = [
       pkgs.mesa
       pkgs-i686.mesa
+      pkgs.mangohud
+      pkgs-i686.mangohud
     ];
   };
 

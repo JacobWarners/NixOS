219fe381936759b7f80d0f7bf16fda59e3ea93a4
NanoBlazer915 <jacob.warners@weka.io>
2025-08-22 09:29:25 -0700

    maybe-Aug-22-2025

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -26,6 +26,8 @@
   };
     xdg.mime.defaultApplications = {
     "x-scheme-handler/zoommtg" = "us.zoom.Zoom.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
   };
 
   # System-wide packages typically used in a Hyprland environment.

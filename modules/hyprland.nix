b46581307277c0b38eefaeb09804bf3ca5aa26a8
NanoBlazer915 <jacob.warners@weka.io>
2025-08-25 09:24:05 -0700

    wef-Aug-25-2025

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -29,17 +29,15 @@
 
     # This configuration explicitly forces the hyprland portal backend.
     # It addresses the D-Bus errors by ensuring the correct backend is used.
-      config.common.default = [ "hyprland" "gtk" ];
+    config = {
+      common = {
+        "org.freedesktop.portal.Filer" = "gtk";
+        "org.freedesktop.portal.FileChooser" = "gtk";
+        "org.freedesktop.portal.Request" = "hyprland";
+        "org.freedesktop.portal.Screenshot" = "hyprland";
       };
-#    config = {
-#      common = {
-#        "org.freedesktop.portal.Filer" = "hyprland";
-#        "org.freedesktop.portal.FileChooser" = "hyprland";
-#        "org.freedesktop.portal.Request" = "hyprland";
-#        "org.freedesktop.portal.Screenshot" = "hyprland";
-#      };
-#    };
-#  };
+    };
+  };
 
   # === Default Application Handlers ===
   # This block is for setting default applications for specific protocols.

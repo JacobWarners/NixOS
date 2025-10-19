fa2ed4548e39ca9a919dbeb8d5a7b89c25ab54e1
NanoBlazer915 <jacob.warners@weka.io>
2025-10-19 11:37:19 -0700

    f-Oct-19-2025

--- modules/undock-service.nix
+++ modules/undock-service.nix
@@ -2,6 +2,13 @@
 { config, pkgs, ... }:
 
 {
+  # -- eGPU Hot-Unplug Recovery (Final Direct Execution Method) --
+
+  # 1. Enable TTYs so we have a recovery target.
+
+  # 2. The complete udev rule. It bypasses the deadlocked systemd service
+  #    manager by executing the recovery commands DIRECTLY when the
+  #    hardware event is detected.
   services.udev.extraRules = ''
     ACTION=="unbind", DEVPATH=="/devices/pci0000:00/0000:00:01.2/0000:60:00.0/0000:61:04.0", RUN+="${pkgs.coreutils}/bin/sh -c '(${pkgs.procps}/bin/pkill -9 -f ''^${pkgs.hyprland}/bin/Hyprland'' && ${pkgs.coreutils}/bin/sleep 1 && ${pkgs.kbd}/bin/con2fbmap 2 0 && ${pkgs.kbd}/bin/chvt 2) &'"
   '';

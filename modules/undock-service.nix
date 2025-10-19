1dfab31ac613ccc20112b2d15d39be7560beffc1
NanoBlazer915 <jacob.warners@weka.io>
2025-10-19 11:42:15 -0700

    fsd-Oct-19-2025

--- modules/undock-service.nix
+++ modules/undock-service.nix
@@ -1,17 +1,33 @@
-# This is the complete file.
 { config, pkgs, ... }:
 
+let
+  # 1. Define our recovery logic in a self-contained script.
+  #    NixOS will build this script and all the paths to pkill, sleep, etc.,
+  #    will be hardcoded and correct.
+  recoveryScript = pkgs.writeShellScript "egpu-undock-recovery" ''
+    #! ${pkgs.bash}/bin/bash
+    (
+      # The parentheses run this whole block in a subshell,
+      # which is good practice for detaching.
+      ${pkgs.procps}/bin/pkill -9 -f '^${pkgs.hyprland}/bin/Hyprland'
+      ${pkgs.coreutils}/bin/sleep 1
+      ${pkgs.kbd}/bin/con2fbmap 2 0
+      ${pkgs.kbd}/bin/chvt 2
+    ) &
+  '';
+
+in
 {
-  # -- eGPU Hot-Unplug Recovery --
+  # -- eGPU Hot-Unplug Recovery (Robust Script Method) --
 
-  # 1. Enable TTYs so we have a recovery target.
 
-  # 2. Grant the udev daemon access to the programs we need to run.
-  #    This is REQUIRED by NixOS for security and is what fixes the build error.
-  services.udev.packages = [ pkgs.coreutils pkgs.procps pkgs.kbd ];
+  # 2. Grant udev access to bash, which is needed to RUN our script.
+  #    We no longer need to list the other packages here because their paths
+  #    are baked directly into the script itself.
+  services.udev.packages = [ pkgs.bash ];
 
-  # 3. The udev rule that triggers our recovery script directly.
+  # 3. The udev rule is now extremely simple. It just calls our script.
   services.udev.extraRules = ''
-    ACTION=="unbind", DEVPATH=="/devices/pci0000:00/0000:00:01.2/0000:60:00.0/0000:61:04.0", RUN+="${pkgs.coreutils}/bin/sh -c \"(${pkgs.procps}/bin/pkill -9 -f '^${pkgs.hyprland}/bin/Hyprland' && ${pkgs.coreutils}/bin/sleep 1 && ${pkgs.kbd}/bin/con2fbmap 2 0 && ${pkgs.kbd}/bin/chvt 2) &\""
+    ACTION=="unbind", DEVPATH=="/devices/pci0000:00/0000:00:01.2/0000:60:00.0/0000:61:04.0", RUN+="${recoveryScript}"
   '';
 }

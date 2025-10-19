069c1cb537ccbf9049a72ef9d9c545485caeaf00
NanoBlazer915 <jacob.warners@weka.io>
2025-10-19 10:24:57 -0700

    adf-Oct-19-2025

--- modules/undock-service.nix
+++ modules/undock-service.nix
@@ -1,19 +1,19 @@
-
 { config, pkgs, ... }:
 {
+  # -- eGPU Hot-Unplug Recovery (Final Path Fix) --
+
+  services.openssh.enable = true;
 
   systemd.services.kill-hyprland-on-undock = {
     description = "Forcefully terminate Hyprland session for eGPU undock recovery.";
     serviceConfig = {
       Type = "oneshot";
+      # THE FIX IS HERE: We now use the full path to pkill.
       ExecStart = "${pkgs.procps}/bin/pkill -9 -x -f '.Hyprland-wrapped'";
     };
   };
 
   services.udev.extraRules = ''
-    # Target the 'unbind' action on the exact DEVPATH of the parent Thunderbolt PCI bridge.
-    # This is immune to the race condition that was causing previous attempts to fail.
     ACTION=="unbind", DEVPATH=="/devices/pci0000:00/0000:00:01.2/0000:60:00.0/0000:61:04.0", TAG+="systemd", ENV{SYSTEMD_WANTS}+="kill-hyprland-on-undock.service"
   '';
-
 }

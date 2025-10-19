68267fd1583370abd56fc4f9939ced0d39feadcc
NanoBlazer915 <jacob.warners@weka.io>
2025-10-19 11:40:19 -0700

    sdf-Oct-19-2025

--- modules/undock-service.nix
+++ modules/undock-service.nix
@@ -1,15 +1,16 @@
-# /path/to/your/egpu-recovery.nix
+# This is the complete file.
 { config, pkgs, ... }:
 
 {
-  # -- eGPU Hot-Unplug Recovery (Final Buildable Direct Execution Method) --
+  # -- eGPU Hot-Unplug Recovery --
 
-  # CRITICAL: This line tells the NixOS builder that the programs from
-  # these packages are allowed to be used in udev RUN commands.
-  # This will fix the build error.
+  # 1. Enable TTYs so we have a recovery target.
+
+  # 2. Grant the udev daemon access to the programs we need to run.
+  #    This is REQUIRED by NixOS for security and is what fixes the build error.
   services.udev.packages = [ pkgs.coreutils pkgs.procps pkgs.kbd ];
 
-  # The udev rule now has the necessary permissions to be built correctly.
+  # 3. The udev rule that triggers our recovery script directly.
   services.udev.extraRules = ''
     ACTION=="unbind", DEVPATH=="/devices/pci0000:00/0000:00:01.2/0000:60:00.0/0000:61:04.0", RUN+="${pkgs.coreutils}/bin/sh -c \"(${pkgs.procps}/bin/pkill -9 -f '^${pkgs.hyprland}/bin/Hyprland' && ${pkgs.coreutils}/bin/sleep 1 && ${pkgs.kbd}/bin/con2fbmap 2 0 && ${pkgs.kbd}/bin/chvt 2) &\""
   '';

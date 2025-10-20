ade45a54042c80d63d2489937e1b310a367b05b4
NanoBlazer915 <jacob.warners@weka.io>
2025-10-20 13:11:00 -0700

    Refactored nix to make polkit in another file-Oct-20-2025

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./modules/sonic-waygame.nix
+    ./modules/polkit.nix
     ./modules/telegraf.nix
     #    ./modules/undock-service.nix
     #    ./modules/specialisation.nix
---
+++ modules/polkit.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+{
+security.polkit.extraConfig = ''
+  polkit.addRule(function(action, subject) {
+    // Rule for CoreCtrl
+    if ((action.id == "org.corectrl.helper.init" ||
+         action.id == "org.corectrl.helperkiller.init") &&
+        subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+    }
+
+    // Rule for nmcli, checking BOTH the symlink and the real store path
+    if (action.id == "org.freedesktop.policykit.exec" &&
+        subject.isInGroup("wheel") &&
+        (action.lookup("program") == "'' + pkgs.networkmanager + ''/bin/nmcli" ||
+         action.lookup("program") == "/run/current-system/sw/bin/nmcli")) {
+      return polkit.Result.YES;
+    }
+  });
+
+  '';
+} 

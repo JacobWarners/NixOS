d6da2b32633c693a09fe4f3a5b6275f24bf88958
NanoBlazer915 <jacob.warners@weka.io>
2025-10-19 22:22:00 -0700

    kek-Oct-19-2025

--- modules/audio.nix
+++ modules/audio.nix
@@ -10,8 +10,17 @@
       support32Bit = true;
     };
     pulse.enable = true;
+
+    # Add this block to declaratively create the virtual sink
+    pulse.extraConfig = ''
+      # Create a virtual sink for error sounds
+      load-module module-null-sink sink_name=error_sounds sink_properties=device.description="Error_Sounds"
+
+      # Route its output back to the default hardware sink
+      load-module module-loopback source=error_sounds.monitor sink=@DEFAULT_SINK@
+    '';
+
     # Uncomment if you need JACK support
     # jack.enable = true;
   };
 }
-

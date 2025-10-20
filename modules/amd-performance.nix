ce9042ce5269b20aed4ae1698b71aae182419a68
NanoBlazer915 <jacob.warners@weka.io>
2025-10-20 11:39:15 -0700

    Adding polkit rule to amd-perf.nix-Oct-20-2025

--- modules/amd-performance.nix
+++ modules/amd-performance.nix
@@ -9,15 +9,23 @@
   
   # Allow users to control GPU settings
   security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if ((action.id == "org.corectrl.helper.init" ||
-           action.id == "org.corectrl.helperkiller.init") &&
-          subject.local == true &&
-          subject.active == true &&
-          subject.isInGroup("wheel")) {
-            return polkit.Result.YES;
-      }
-    });
+  polkit.addRule(function(action, subject) {
+    // Rule to allow users in "wheel" group to control CoreCtrl
+    if ((action.id == "org.corectrl.helper.init" ||
+         action.id == "org.corectrl.helperkiller.init") &&
+        subject.local == true &&
+        subject.active == true &&
+        subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+    }
+
+    // Rule to allow users in "wheel" group to manage networks
+    if (action.id == "org.freedesktop.NetworkManager.network-control" &&
+        subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+    }
+  });
+
   '';
   
   # Enable performance governor

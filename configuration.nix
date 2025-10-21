1b4ee2284456b6507226ab4b56ce639ec815a9ab
NanoBlazer915 <jacob.warners@weka.io>
2025-10-21 14:33:57 -0700

    fe-Oct-21-2025

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./modules/sonic-waygame.nix
-    ./modules/virtual-audio-sink.nix
+    #    ./modules/virtual-audio-sink.nix
     ./modules/polkit.nix
     ./modules/telegraf.nix
     #    ./modules/undock-service.nix

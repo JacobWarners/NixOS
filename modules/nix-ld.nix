cdcda1f2d77a50a3ba31d49e94d91990078d97f7
NanoBlazer915 <jacob.warners@weka.io>
2026-01-02 20:49:03 -0800

    Tibia again?-Jan-02-2026

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,12 +1,24 @@
 { config, pkgs, nix-ld, ... }:
 
+let
+  # We import the previous stable release (24.05) specifically to get 
+  # the older libxml2.so.2 that Tibia needs.
+  pkgs2405 = import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/nixos-24.05.tar.gz";
+    sha256 = "sha256:1lr1uep29mln40gq1n63dqy5h9g6q797698q98740f9l1kqf9c6d";
+  }) { system = pkgs.system; };
+in
 {
   programs.nix-ld.enable = true;
 
   programs.nix-ld.libraries = with pkgs; [
+    # --- The Fix: Use the old version for this specific library ---
+    pkgs2405.libxml2
+    # --------------------------------------------------------------
+
     libxslt
     glibc
-    libxml2
+    # libxml2  <-- Removed the system version to avoid conflicts
     libsForQt5.qtwayland
     nss
     nspr
@@ -36,43 +48,11 @@
     libxkbcommon
     libglvnd
     dbus
-    nss
-    nspr
+    # nss  <-- Removed duplicate (you had it listed twice)
+    # nspr <-- Removed duplicate (you had it listed twice)
     xorg.libXcomposite
     xorg.libXdamage
     libevent
     libdrm
   ];
 }
-#{
-#  imports = [
-#    nix-ld.nixosModules.nix-ld
-#  programs.nix-ld.enable = true;
-#  ];
-##programs.nix-ld.libraries = with pkgs; [
-#    glibc
-#    zlib
-##    libstdcxx5
-#    openssl
-#    nss
-#    nspr
-##    libX11
-##    libxcb
-#    xcbutil
-#    xcbutilcursor
-#    xcbutilrenderutil
-#    libxkbcommon
-#    fontconfig
-#    freetype
-#    dbus
-#    libglvnd
-#    brotli.lib
-#    wayland
-#    gtk2
-#    stdenv.cc.cc.lib
-#    brotli
-#    xorg.libX11
-#    libglvnd
-#  ];
-#}
-#

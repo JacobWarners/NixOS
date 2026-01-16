bd3549d78b67d7bb30c2a65cf226b4ffcf04f510
NanoBlazer915 <jacob.warners@weka.io>
2026-01-16 14:31:36 -0800

    maybe virt?-Jan-16-2026

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,78 +1,45 @@
 { config, pkgs, ... }:
 
 {
-  # Install virtualization packages and additional utilities for Spice
-  environment.systemPackages = with pkgs; [
-    qemu_kvm # QEMU with KVM support
-    libvirt # Libvirt library
-    virt-manager # GUI tool for managing virtual machines
-    spice-vdagent # Agent to enable clipboard sharing and file transfer
-    spice # Spice client and support libraries
-    spice-protocol
-    spice-gtk
-    libguestfs
-  ];
-
+  # 1. Enable the Libvirt daemon (the backend)
   virtualisation.libvirtd = {
     enable = true;
-    qemu.ovmf = {
-      enable = true;
-      packages = [
-        (pkgs.OVMFFull.override {
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true; # Enable TPM for Windows 11 support
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
           secureBoot = true;
-        }).fd
-      ];
+          tpmSupport = true;
+        }).fd];
+      };
     };
   };
-  # Enable and configure libvirtd service
-  #  virtualisation.libvirtd = {
-  #    enable = true;
-  #    qemu.ovmf = {
-  #      enable = true;
-  #      (pkgs.OVMFFull.override {
-  #        secureBoot = true;
-  #        csmSupport = false;
-  ##      packages = [pkgs.OVMF.fd];
-  #        }).fd
-  #    };
-  #  };
-  #
 
-  #    system.activationScripts.ovmfCopy = {
-  #    text = ''
-  #      # Create a directory for the OVMF firmware in a writable location.
-  #      mkdir -p /run/libvirt/nix-ovmf
-  #      rm -rf /run/libvirt/nix-ovmf/*
-  #
-  #      # Copy the firmware files from the OVMF derivation.
-  #      cp ${pkgs.OVMF.fd}/FV/OVMF_CODE.fd /run/libvirt/nix-ovmf/OVMF_CODE.fd
-  #      cp ${pkgs.OVMF.fd}/FV/OVMF_VARS.fd /run/libvirt/nix-ovmf/OVMF_VARS.fd
-  #
-  #      # (Optional) Set appropriate permissions, though they should be readable.
-  #      chmod 444 /run/libvirt/nix-ovmf/OVMF_CODE.fd /run/libvirt/nix-ovmf/OVMF_VARS.fd
-  #    '';
-  #  };
-  #    system.activationScripts.ovmfSecure = {
-  #    text = ''
-  #      mkdir -p /var/lib/libvirt/firmware
-  #      rm -rf /var/lib/libvirt/firmware/*
-  #
-  #      cp ${pkgs.OVMFFull.fd}/FV/OVMF_CODE.secboot.fd /var/lib/libvirt/firmware/OVMF_CODE.secboot.fd
-  #      cp ${pkgs.OVMFFull.fd}/FV/OVMF_VARS.secboot.fd /var/lib/libvirt/firmware/OVMF_VARS.secboot.fd
-  #
-  #      chmod 444 /var/lib/libvirt/firmware/OVMF_CODE.secboot.fd /var/lib/libvirt/firmware/OVMF_VARS.secboot.fd
-  #    '';
-  #  };
-  # Enable Spice services for clipboard sharing and file transfer support
-  services.spice-vdagentd.enable = true;
+  # 2. Enable the Virt-Manager GUI
+  programs.virt-manager.enable = true;
 
-  # Enable necessary hardware acceleration and video support for Spice
-  hardware.graphics = {
-    extraPackages = with pkgs; [
-      vaapiIntel # VAAPI for Intel GPUs
-      vaapiVdpau # VDPAU/VAAPI interoperability
-    ];
+  # 3. System Packages
+  # 'libvirt' provides 'virsh' which your shell script needs
+  environment.systemPackages = with pkgs; [
+    libvirt
+    qemu
+    OVMFFull  # UEFI firmware
+    swtpm     # TPM emulation
+  ];
+
+  # 4. Polkit (Required for Hyprland/Window Managers)
+  # Without this, virt-manager will fail to authenticate because there is no 
+  # graphical password prompt agent running by default in raw Hyprland.
+  security.polkit.enable = true;
+  
+  # Optional: If you use a dark theme, force GTK apps (like virt-manager) 
+  # to use it. Remove if you handle GTK theming elsewhere.
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
   };
-
 }
-

3c6df6609b81934dc4f72b96b736d8cbb586eeb3
NanoBlazer915 <jacob.warners@weka.io>
2026-01-16 14:34:00 -0800

    wef-Jan-16-2026

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,39 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  # 1. Enable the Libvirt daemon (the backend)
+  # 1. Enable the Libvirt daemon
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
       runAsRoot = true;
-      swtpm.enable = true; # TPM support
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
+      swtpm.enable = true; # TPM support (Essential for Windows 11)
+      # REMOVED: ovmf = { ... } (This is now handled automatically by NixOS)
     };
   };
 
-  # 2. Enable the Virt-Manager GUI
+  # 2. Enable Virt-Manager
   programs.virt-manager.enable = true;
 
   # 3. System Packages
   environment.systemPackages = with pkgs; [
     libvirt
     qemu
-    OVMFFull
     swtpm
   ];
 
-  # 4. Polkit (Critical for Hyprland users)
-  # Allows virt-manager to ask for your password when connecting to system libvirt
+  # 4. Polkit (Critical for Hyprland/WM authentication)
   security.polkit.enable = true;
 
-  # 5. Enable dconf
-  # Virt-manager requires this to store settings (connections, view preferences, etc.)
+  # 5. Enable dconf (Required for virt-manager to save settings)
   programs.dconf.enable = true;
 }

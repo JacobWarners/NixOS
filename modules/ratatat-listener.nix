a40270cd4f33f19e2df3286009131d204d6313ba
NanoBlazer915 <jacob.warners@weka.io>
2026-01-20 10:49:19 -0800

    wef-Jan-20-2026

--- modules/ratatat-listener.nix
+++ modules/ratatat-listener.nix
@@ -2,7 +2,7 @@
 
 let
   user = "jake";
-  # PASS AS A STRING: Nix won't check if this exists during build
+  # PASS AS A STRING
   binaryPath = "/home/jake/Documents/Code/Rust/ratatat-rust/target/release/ratatat-listener";
 in
 {
@@ -11,13 +11,18 @@ in
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
 
-    serviceConfig = {
-      # We still use the loader trick to run the non-Nix binary
-      ExecStart = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2 ${binaryPath}";
+    # We use a script to ensure the binary is patched before running
+    # This keeps your source folder clean but makes it work on NixOS
+    script = ''
+      # 1. Define where the dynamic loader is on YOUR system
+      LOADER="${pkgs.glibc}/lib/ld-linux-x86-64.so.2"
       
-      # We need to give it the libraries it expects (libc, etc)
-      Environment = "LD_LIBRARY_PATH=${lib.makeLibraryPath [ pkgs.stdenv.cc.cc pkgs.glibc ]}";
+      # 2. Run the binary using the loader explicitly
+      # This forces the binary to use the NixOS loader, ignoring what it was built with.
+      exec $LOADER "${binaryPath}"
+    '';
 
+    serviceConfig = {
       User = user;
       Group = "users";
       Restart = "always";

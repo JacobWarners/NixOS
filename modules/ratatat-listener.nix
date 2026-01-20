75aed8b39129569bb9d13bfc8f6359226d995576
NanoBlazer915 <jacob.warners@weka.io>
2026-01-20 10:10:50 -0800

    ratatnow?-Jan-20-2026

--- modules/ratatat-listener.nix
+++ modules/ratatat-listener.nix
@@ -1,14 +1,21 @@
 { config, pkgs, lib, ... }:
 
 let
-  user = "jake"; # Define your user here
+  user = "jake";
   projectDir = "/home/${user}/Documents/Code/Rust/ratatat-rust";
   binaryPath = "${projectDir}/target/release/ratatat-rust";
 in
 {
+  # --- FIX STARTS HERE ---
+  # We must explicitly tell NixOS that this service option exists.
+  options.services.ratatat-listener = {
+    enable = lib.mkEnableOption "Ratatat Keyboard Listener";
+  };
+  # --- FIX ENDS HERE ---
+
   config = lib.mkIf config.services.ratatat-listener.enable {
     
-    # 1. Allow 'jake' to read keyboard events (replacing root requirement)
+    # 1. Permission for 'jake' to read /dev/input
     users.users.${user}.extraGroups = [ "input" ];
 
     systemd.services.ratatat-listener = {
@@ -18,21 +25,21 @@ in
       path = with pkgs; [ 
         mpg123      
         procps      
-        pulseaudio # Provides commands, but 'jake' uses the socket
+        pulseaudio 
       ];
 
       serviceConfig = {
         ExecStart = binaryPath;
-        
-        # 2. Run as YOU, not root.
-        # This fixes the "Jack server/Connection refused" audio errors.
         User = user;
-        
-        # We set this, but the absolute path in Rust makes it redundant (good safety).
         WorkingDirectory = projectDir;
-
         Restart = "always";
         RestartSec = "5s";
+
+        # 2. Audio Environment Variables (So the service finds your speakers)
+        Environment = [
+          "XDG_RUNTIME_DIR=/run/user/1000"
+          "PULSE_SERVER=unix:/run/user/1000/pulse/native"
+        ];
       };
     };
   };

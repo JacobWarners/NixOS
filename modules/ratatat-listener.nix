55ddc34b9d61d917426f79f12d050f689464760c
NanoBlazer915 <jacob.warners@weka.io>
2026-01-20 16:00:53 -0800

    nowway-Jan-20-2026

--- modules/ratatat-listener.nix
+++ modules/ratatat-listener.nix
@@ -2,42 +2,30 @@
 
 let
   projectRoot = "/home/jake/Documents/Code/Rust/ratatat-rust";
-  binaryPath = "${projectRoot}/target/release/ratatat-rust";
-
-  # 1. We need the plugins so ALSA apps can talk to Pulse/Pipewire
-  alsaPlugins = pkgs.alsa-plugins;
-  
-  libraryPath = lib.makeLibraryPath [
-    pkgs.stdenv.cc.cc.lib
-    pkgs.openssl
-    pkgs.alsa-lib
-    pkgs.glibc
-    pkgs.pulseaudio
-  ];
+  # We point to the binary, but we will run it via nix-shell
+  binaryPath = "target/release/ratatat-rust";
 in
 {
   systemd.user.services.ratatat-listener = {
-    description = "Ratatat Listener Service (User Session)";
+    description = "Ratatat Listener Service (Nix-Shell Wrapper)";
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
 
-    environment = {
-      LD_LIBRARY_PATH = "${libraryPath}";
-      
-      # 2. FORCE the application to use the Pulse backend for ALSA
-      ALSA_OUTPUT_DRIVER = "pulse";
-      
-      # 3. Tell it where the plugins are (Critical for NixOS)
-      ALSA_PLUGIN_DIRS = "${alsaPlugins}/lib/alsa-lib";
-    };
-
-    script = ''
-      LOADER="${pkgs.glibc}/lib/ld-linux-x86-64.so.2"
-      exec $LOADER "${binaryPath}"
-    '';
-
     serviceConfig = {
+      # 1. Run exactly like your manual test:
+      #    - Enter nix-shell with audio libs
+      #    - Export the library path (Critical!)
+      #    - Run the binary using the loader trick
+      ExecStart = ''
+        ${pkgs.nix}/bin/nix-shell -p alsa-lib openssl pulseaudio glibc --run "
+          export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH
+          ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 ./${binaryPath}
+        "
+      '';
+
+      # 2. Run inside the project root so it finds 'Loud-pipes.mp3'
       WorkingDirectory = projectRoot;
+      
       Restart = "always";
       RestartSec = "5s";
     };

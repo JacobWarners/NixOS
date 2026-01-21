dd7265e550ee38881f79d572a55109b9086bab02
NanoBlazer915 <jacob.warners@weka.io>
2026-01-20 16:03:39 -0800

    -Jan-20-2026

--- modules/ratatat-listener.nix
+++ modules/ratatat-listener.nix
@@ -1,30 +1,53 @@
 { config, lib, pkgs, ... }:
 
 let
-  # Your project folder
+  # 1. PATHS
   projectRoot = "/home/jake/Documents/Code/Rust/ratatat-rust";
-  # Relative path to binary inside that folder
-  binaryRelativePath = "./target/release/ratatat-rust";
+  binaryPath = "${projectRoot}/target/release/ratatat-rust";
+
+  # 2. LIBRARIES & PLUGINS (The stuff Nix used to do for you)
+  # We explicitly grab the ALSA plugins so audio works.
+  alsaPluginDir = "${pkgs.alsa-plugins}/lib/alsa-lib";
+  
+  libPath = lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc.lib
+    pkgs.openssl
+    pkgs.alsa-lib
+    pkgs.glibc
+    pkgs.pulseaudio
+  ];
+
+  # 3. THE WRAPPER SCRIPT
+  # We create a script in the Nix store that sets up the environment 
+  # and THEN runs your binary. This is what 'ExecStart' will point to.
+  runnerScript = pkgs.writeShellScript "ratatat-runner" ''
+    # Set the library path so the binary finds .so files
+    export LD_LIBRARY_PATH="${libPath}:$LD_LIBRARY_PATH"
+    
+    # Set the ALSA plugin directory (CRITICAL for audio on NixOS)
+    export ALSA_PLUGIN_DIRS="${alsaPluginDir}"
+    
+    # Force PulseAudio backend just in case
+    export ALSA_OUTPUT_DRIVER=pulse
+    
+    # Run the binary using the dynamic loader
+    exec ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 "${binaryPath}"
+  '';
+
 in
 {
   systemd.user.services.ratatat-listener = {
-    description = "Ratatat Listener Service (Nix-Shell Wrapper)";
+    description = "Ratatat Listener Service (Wrapper)";
+    
+    # Start after graphical session is up
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
 
     serviceConfig = {
-      # === THE FIX ===
-      # Instead of running the binary directly, we spawn a nix-shell.
-      # This guarantees the environment matches your successful manual test 100%.
-      ExecStart = ''
-        ${pkgs.nix}/bin/nix-shell -p alsa-lib openssl pulseaudio glibc --run "
-          export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH
-          # Use the dynamic loader to run the binary
-          ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 ${binaryRelativePath}
-        "
-      '';
-
-      # Run inside the folder so it finds 'Loud-pipes.mp3'
+      # Point Systemd to our clean wrapper script
+      ExecStart = "${runnerScript}";
+      
+      # Run inside the project root so it finds 'Loud-pipes.mp3'
       WorkingDirectory = projectRoot;
       
       Restart = "always";

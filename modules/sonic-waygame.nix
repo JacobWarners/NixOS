5d8af9916858e6331e358035c233138576eb1f4b
NanoBlazer915 <jacob.warners@weka.io>
2026-02-26 11:30:52 -0800

    sonci better-Feb-26-2026

--- modules/sonic-waygame.nix
+++ modules/sonic-waygame.nix
@@ -12,7 +12,7 @@
 
     # "serviceConfig" replaces the [Service] section
     serviceConfig = {
-      ExecStart = "/home/jake/Documents/Code/sonic-waygame/key_counter_daemon/target/release/key_counter_daemon --normal";
+      ExecStart = "/home/jake/Documents/Code/Rust/key_counter_daemon/target/release/key_counter_daemon --normal";
       
       # Ensure libnotify (notify-send) and hyprland (hyprctl) are in the path
       Environment = "PATH=${pkgs.libnotify}/bin:${pkgs.hyprland}/bin:/run/current-system/sw/bin:/usr/bin";
